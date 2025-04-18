ef29b3690a47ad07c12c4581df3b2a0dfaaf1c20
renatomarf76 <renatomarf76@egmail.com>
2025-04-18 16:03:58 -0300

    05 - Search Filters

---
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -0,0 +1,22 @@
+import { SearchIcon } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+
+interface Props {
+  disabled?: boolean;
+};
+
+export const SearchInput = ({
+  disabled,
+}: Props) => {
+  return ( 
+    <div className="flex items-center gap-2 w-full">
+      <div className="relative w-full">
+        <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500"/>
+        <Input className="pl-8" style={{ paddingLeft: 32 }} placeholder="Search products" disabled={disabled} />
+      </div>
+      {/* TODO: Add categoires view all button */}
+      {/* TODO: Ad library button */}
+    </div>
+   );
+};
